a081d7d5a0ab9dc51c5ee8357bd1c3120569ab3e
Geffen <geffen.posner@kubiya.ai>
2024-11-05 18:28:50 +0200

    create knowledge tool v1 - hardcoded owner uuid

---
+++ tools/knowledge_tools/main.py
@@ -0,0 +1,90 @@
+import argparse
+import os
+import requests
+
+def create_knowledge(
+        title: str,
+        description: str,
+        tags: list[str],
+        content: str,
+        teammates_with_access: list[str],
+        user_groups_with_access: list[str],
+):
+    print({
+            "name": title,
+            "description": description,
+            "labels": tags,
+            "content": content,
+            "type": "knowledge",
+            "source": "manual",
+            "properties": {},
+            "supported_agents": teammates_with_access,
+            "groups": user_groups_with_access,
+        })
+
+
+    print("Sending request to Kubiya API...")
+    response = requests.post(
+        'https://api.kubiya.ai/api/v1/knowledge',
+        headers={
+            'Authorization': f'UserKey {os.environ["geff_test"]}',
+            'Content-Type': 'application/json'
+        },
+        json={
+            "name": title,
+            "description": description,
+            "labels": tags,
+            "content": content,
+            "type": "knowledge",
+            "source": "manual",
+            "properties": {},
+            "supported_agents": teammates_with_access,
+            "groups": user_groups_with_access,
+            "supported_agents_groups": [],
+            "owner": "5851e879-552a-4209-bd60-f33f11525d82",
+            "uuid": ""
+        }
+    )
+    print(response.json())
+    if response.status_code >= 400:
+        print(f"Error: {response.status_code} {response.text}")
+        return
+    
+    base_url = "https://app.kubiya.ai/knowledge"
+    try:
+        knowledge_id = response.json()["uuid"]
+        url = f"{base_url}/{knowledge_id}"
+        print("Knowledge created successfully! Link: ", url)
+    except Exception:
+        print(f"Knowledge created successfully! You can find it in {base_url}")
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Create a piece of Kubiya knowledge with {content}!")
+    parser.add_argument("title", help="title of the piece of knowledge")
+    parser.add_argument("description", help="description of the piece of knowledge")
+    parser.add_argument("tags", help="tags related to the piece of knowledge. multiple tags should be comma separated")
+    parser.add_argument("content", help="content of the piece of knowledge")
+    parser.add_argument("teammates_with_access", help="Kubiya teammates with access to the piece of knowledge")
+    parser.add_argument("user_groups_with_access", help="groups of Kubiya users with access to the piece of knowledge")
+
+    # Parse command-line arguments
+    args = parser.parse_args()
+
+    # Get coordinates for the given city
+    title = args.title
+    description = args.description
+    tags = args.tags.split(",") if args.tags else []
+    content = args.content
+    teammates_with_access = args.teammates_with_access.split(",") if args.teammates_with_access else []
+    user_groups_with_access = args.user_groups_with_access.split(",") if args.user_groups_with_access else []
+
+    create_knowledge(
+        title=title,
+        description=description,
+        tags=tags,
+        content=content,
+        teammates_with_access=teammates_with_access,
+        user_groups_with_access=user_groups_with_access,
+    )
---
+++ tools/knowledge_tools/tool_def.py
@@ -0,0 +1,49 @@
+import inspect
+
+from kubiya_sdk.tools.models import Arg, Tool, FileSpec
+from kubiya_sdk.tools.registry import tool_registry
+
+from . import main
+
+create_knowledge_tool = Tool(
+    name="create_knowledge",
+    type="docker",
+    image="python:3.12",
+    description="Creates a piece of Kubiya knowldege with {content}!",
+    args=[
+        Arg(name="title", description="title of the piece of knowledge", required=True),
+        Arg(name="description", description="description of the piece of knowledge", required=True),
+        Arg(name="tags", description="tags related to the piece of knowledge. multiple tags should be comma separated", required=False),
+        Arg(name="content", description="content of the piece of knowledge", required=True),
+        Arg(name="teammates_with_access", description="Kubiya teammates with access to the piece of knowledge", required=True),
+        Arg(name="user_groups_with_access", description="groups of Kubiya users with access to the piece of knowledge", required=True),
+          ],
+    on_build="""
+curl -LsSf https://astral.sh/uv/install.sh | sh > /dev/null 2>&1
+. $HOME/.cargo/env
+
+uv venv > /dev/null 2>&1
+. .venv/bin/activate > /dev/null 2>&1
+
+if [ -f /tmp/requirements.txt ]; then
+    uv pip install -r /tmp/requirements.txt > /dev/null 2>&1
+fi
+""",
+    content="""
+python /tmp/main.py "{{ .name }}"
+""",
+    with_files=[
+        FileSpec(
+            destination="/tmp/main.py",
+            content=inspect.getsource(main),
+        ),
+        # Add any requirements here if needed
+        # FileSpec(
+        #     destination="/tmp/requirements.txt",
+        #     content="",
+        # ),
+    ],
+    secrets=["geff_test"]
+)
+
+tool_registry.register(create_knowledge_tool)
